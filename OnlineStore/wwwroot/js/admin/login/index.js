52ed747f6427fa1ef6600f475482bdbdfbac9bfb
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-04-21 15:21:57 +0700

    validate required for login and register page

--- OnlineStore/wwwroot/js/admin/login/index.js
+++ OnlineStore/wwwroot/js/admin/login/index.js
@@ -3,98 +3,15 @@
     var init = function () {
         $(document).ready(function() {
             onValidateForm();
-            //onRegister();
         });
     };
     // #endregion
 
-    // #region Events
-    //var onRegister = function () {
-    //    //$('#frmAdminLogin').validate({
-    //    //    errorClass: 'red',
-    //    //    ignore: [],
-    //    //    lang: 'en',
-    //    //    rules: {
-    //    //        userName: {
-    //    //            required: true
-    //    //        },
-    //    //        password: {
-    //    //            required: true
-    //    //        }
-    //    //    }
-    //    //});
-    //    $('#btnLogin').on('click',
-    //        function (event) {
-    //            event.preventDefault();
-    //            var email = $('#txtEmail').val();
-    //            var password = $('#txtPassword').val();
-    //            //loginFunc(email, password);
-    //            $.ajax({
-    //                type: "POST",
-    //                url: "/Login/Index",
-    //                beforeSend: function (xhr) {
-    //                    xhr.setRequestHeader("XSRF-TOKEN",
-    //                        $('input:hidden[name="__RequestVerificationToken"]').val());
-    //                },
-    //                data: {
-    //                    Email: email,
-    //                    Password: $('#txtPassword').val()
-    //                },
-    //                //contentType: "text",
-    //                //dataType: "json",
-    //                success: function (response) {
-    //                    if (response.success) {
-    //                        window.location.href = "~/Areas/Admin/Pages/Home/Index";
-    //                    } else {
-    //                        commons.notify('Đăng nhập không đúng', 'error');
-    //                    }
-    //                },
-    //                failure: function (response) {
-    //                    console.log(response);
-    //                },
-    //                complete: function (res) {
-    //                    console.log(res);
-    //                }
-    //            });
-    //        });
-    //};
-
     var onValidateForm = function () {
         $(document).ready(function() {
             $('#frmLogin').parsley();
-        }
+        });
     };
-
-    //var loginFunc = function (email, pass) {
-    //    $.ajax({
-    //        type: "POST",
-    //        url: "/Login/Index",
-    //        beforeSend: function (xhr) {
-    //            xhr.setRequestHeader("XSRF-TOKEN",
-    //                $('input:hidden[name="__RequestVerificationToken"]').val());
-    //        },
-    //        data: {
-    //            "Email": email,
-    //            "Password": pass
-    //        },
-    //        contentType: "application/json; charset=utf-8",
-    //        dataType: "json",
-    //        success: function (response) {
-    //            if (response.success) {
-    //                window.location.href = "~/Areas/Admin/Pages/Home/Index";
-    //            } else {
-    //                commons.notify('Đăng nhập không đúng', 'error');
-    //            }
-    //        },
-    //        failure: function (response) {
-    //            console.log(response);
-    //        },
-    //        complete: function (res) {
-    //            console.log(res);
-    //        }
-    //    });
-    //};
-    // #endregion
 
     return {
         init
