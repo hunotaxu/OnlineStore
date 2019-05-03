da6567ca9ff18d1639065da2135c4bca2f55bd14
trinhvancongspk <trinhvancongspk@gmail.com>
2019-05-03 17:30:51 +0700

    mới nè

---
+++ OnlineStore/Pages/Home/Index1.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using DAL.Data.Entities;
+using DAL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineStore.Pages.Home
+{
+    public class Index1Model : PageModel
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IEnumerable<Item> Phones { get; set; }
+        public IEnumerable<Item> Laptops { get; set; }
+        public IEnumerable<Item> Tablets { get; set; }
+        public IEnumerable<Item> Accessories { get; set; }
+
+
+        public Index1Model(IItemRepository itemRepository, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _itemRepository = itemRepository;
+        }
+
+        public void OnGet()
+        {
+            var cus = _userManager.GetUserAsync(HttpContext.User).Result;
+            Phones = _itemRepository.GetByCategory(1);
+            Laptops = _itemRepository.GetByCategory(2);
+            Tablets = _itemRepository.GetByCategory(3);
+            Accessories = _itemRepository.GetByCategory(4);
+        }
+    }
+}
