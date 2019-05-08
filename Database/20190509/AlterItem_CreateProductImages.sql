2987338abef5ff92e8103237848b4c69ab0c75c4
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-05-09 00:51:53 +0700

    db change

---
+++ Database/20190509/AlterItem_CreateProductImages.sql
@@ -0,0 +1,25 @@
+﻿use [OnlineStoreDb]
+alter table Item add PromotionPrice decimal(18, 2)
+DROP TABLE ImageProduct;
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProductImages](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Url] [nvarchar](max) NULL,
+	[ItemId] [int] NULL,
+	[Timestamp] [timestamp] NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[Caption] [nvarchar](250) NULL,
+ CONSTRAINT [PK__ImagePro__3214EC079E4A12CA] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ProductImages]  WITH CHECK ADD  CONSTRAINT [FK_ImageProduct_Item] FOREIGN KEY([ItemId])
+REFERENCES [dbo].[Item] ([Id])
+GO
+ALTER TABLE [dbo].[ProductImages] CHECK CONSTRAINT [FK_ImageProduct_Item]
+GO
