9666d6a21b098fe56cff2c599e47fb034bc3631d
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-06-11 17:02:28 +0700

    dynamic avatar and name for admin

---
+++ OnlineStore/Areas/Admin/ViewComponents/AdminLoginStatusViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Data.Entities;
+using DAL.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Areas.Admin.ViewComponents
+{
+    public class AdminLoginStatusViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserRepository _userRepository;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AdminLoginStatusViewComponent(SignInManager<ApplicationUser> signInManager, IUserRepository userRepository, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userRepository = userRepository;
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            if (_signInManager.IsSignedIn(HttpContext.User) && user != null && _userRepository.IsAdmin(user))
+            {
+                return View("LoggedIn", user);
+            }
+            //if (_signInManager.IsSignedIn(HttpContext.User) && _userRepository.IsAdmin(user))
+            //{
+            //    return View("LoggedIn", user);
+            //}
+            return View();
+        }
+    }
+}
