4b532495eae21bcee214dd1453aae1869fbaa038
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-06-25 23:24:35 +0700

    add js file

--- OnlineStore/wwwroot/js/admin/product.js
+++ OnlineStore/wwwroot/js/admin/product.js
@@ -498,7 +498,7 @@ var itemPage = (function () {
 
     function formatNumber(n) {
         // format number 1000000 to 1,234,567
-        return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
 
     function formatCurrency(input, blur) {
---
+++ OnlineStore/wwwroot/js/client/product/search.js
@@ -0,0 +1,23 @@
+﻿var search = (function () {
+    var init = function () {
+        $(document).ready(function () {
+            formatPrice();
+        });
+    };
+
+    var formatPrice = function () {
+        //$("#min-price").on({
+        //    keyup: function () {
+        //        commons.formatCurrency($(this));
+        //    }
+        //    //},
+        //    //blur: function () {
+        //    //    commons.formatCurrency($(this), "blur");
+        //    //}
+        //});
+    };
+
+    return {
+        init
+    };
+})();
