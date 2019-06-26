e4f392e5e6708a31f9699a6e7ef94e207db9da27
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-06-26 12:40:25 +0700

    UI sort by

--- OnlineStore/Pages/Product/Search.cshtml.cs
+++ OnlineStore/Pages/Product/Search.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineStore.Models.ViewModels;
 
 namespace OnlineStore.Pages.Product
 {
     public class SearchModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public SearchProductViewModel SearchProductViewModel { get; set; }
         public void OnGet()
         {
 
