f809841463ef0aa19293c80b2d5a7fe9cd08e3fb
trinhvancongspk <trinhvancongspk@gmail.com>
2019-07-03 16:26:37 +0700

    load cartlayout khi thêm sp ở detail

--- OnlineStore/wwwroot/js/client/layout/header.js
+++ OnlineStore/wwwroot/js/client/layout/header.js
@@ -20,9 +20,10 @@
 
 var loadItemMyCart = (function () {
     var init = function () {
-        loadItemCart();       
+        loadItemCart(); 
+        loadCartLayout();
     };   
-    loadItemCart = function () {       
+    var loadItemCart = function () {       
         $.ajax({
             type: "GET",
             url: "/Cart/NumberOfItemsInCart?handler=LoadNumberItemCart",
@@ -42,6 +43,49 @@ var loadItemMyCart = (function () {
             error: function () {
                 //commons.stopLoading();
                 $('#cart-itemtotal').html(``);
+            }
+        });
+    };
+    var loadCartLayout = function () {
+        var PriceTotaltmp = 0;
+        $.ajax({
+            type: "GET",
+            url: "/Cart/Index?handler=LoadCartLayout",
+            beforeSend: function () {
+                commons.startLoading();
+            },
+            success: function (response) {
+                var render = '';
+                if (response !== undefined) {
+                    $.each(response, function (i, item) {
+                        render += Mustache.render($('#template-cart-layout').html(), {
+                            ItemId: item.itemId,
+                            ProductName: item.productName,
+                            Image: item.image,
+                            Price: `${commons.formatNumber(item.price, 0)}đ`,
+                            //TotalPrice: `${commons.formatNumber(item.price * item.quantity, 0)}đ`,
+                            Quantity: item.quantity
+                        });
+                        PriceTotaltmp += item.price * item.quantity;
+                    });
+                }
+                if (render !== '') {
+                    $('#container-pay-cart-layout').html(render);
+                    $('#PriceTotaltmpLayout').html(`${commons.formatNumber(PriceTotaltmp, 0)}đ`);
+                } else {
+                    $('.error-load-cart-layout').html(`<div style='text-align: center;'><br/><h3 class="productnamecheckout">Không có sản phẩm nào trong giỏ hàng</h3></div>`);
+                }
+                commons.stopLoading();
+            },
+            error: function (response) {
+                if (response !== undefined && response !== '') {
+                    commons.notify(response.responseText, 'error');
+                }
+                else {
+                    commons.notify('Lỗi tải giỏ hàng', 'error');
+                }
+
+                commons.stopLoading();
             }
         });
     };
