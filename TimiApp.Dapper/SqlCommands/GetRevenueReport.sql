d183b2075010e1234d07104a5287e3baed63f1e9
Xuan Huynh <hntxuanit15spkt@gmail.com>
2019-07-17 23:00:40 +0700

    fix thống kê doanh thu

--- TimiApp.Dapper/SqlCommands/GetRevenueReport.sql
+++ TimiApp.Dapper/SqlCommands/GetRevenueReport.sql
@@ -9,24 +9,22 @@ BEGIN
 				CAST(@fromDate as date) as FromDate,
 				CAST(@toDate as date) as ToDate,
                 CAST(o.DateCreated AS DATE) as Date,
-                --sum(oi.Quantity*oi.Price) as Revenue,
-				sum(o.Total) as Revenue,
-                --sum((oi.Quantity*oi.Price)-(oi.Quantity * i.OriginalPrice)) as Profit
-				sum(o.Total - (oi.Quantity * i.OriginalPrice)) as Profit
+				sum(o.Total) as Revenue
+				--sum(o.Total - (oi.Quantity * i.Price)) as Profit
                 from dbo.[Order] o
-                inner join dbo.OrderItem oi
-                on o.Id = oi.OrderId
-                inner join Item i
-                on oi.ItemId  = i.Id
+                --inner join dbo.OrderItem oi
+                --on o.Id = oi.OrderId
+                --inner join Item i
+                --on oi.ItemId  = i.Id
                 where o.DateCreated <= cast(@toDate as date) 
 				AND o.DateCreated >= cast(@fromDate as date)
 				and o.Status = 4
 				and o.IsDeleted = 0
-				and i.IsDeleted = 0
-				and oi.IsDeleted = 0
+				--and i.IsDeleted = 0
+				--and oi.IsDeleted = 0
                 group by o.DateCreated
 END
 go
 select * from dbo.[Order]
-EXEC dbo.GetRevenueDaily @fromDate = '20190501',
-                         @toDate = '20190530' 
+EXEC dbo.GetRevenueDaily @fromDate = '20190703',
+                         @toDate = '20190706' 
